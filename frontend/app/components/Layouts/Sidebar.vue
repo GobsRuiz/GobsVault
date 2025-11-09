6cd46654376beb00c1479ccfcda5c9776bcc0d70
GobsRuiz <gbibisruiz@hotmail.com>
2025-11-08 23:17:01 -0300

    feat(sidebar): Add the links, with icons and style adjustments.

--- frontend/app/components/Layouts/Sidebar.vue
+++ frontend/app/components/Layouts/Sidebar.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="sidebar">
-    <UIcon name="iconamoon:lightning-1" size="20" />
+    <UIcon name="iconamoon:lightning-1" size="40" class="mx-auto relative top-[5px]" />
 
     <nav class="sidebar__nav">
       <NuxtLink 
         v-for="(link, index) in links"
         :key="index"
         :to="link.to"
+        active-class="active"
         class="sidebar__nav__link"
       >
         <UIcon :name="link.icon" size="20" />
@@ -19,25 +20,35 @@
 const links = [
   {
     to: "/",
-    icon: "material-symbols:home",
-    text: "Home",
+    icon: "si:dashboard-line",
+    text: "OVERVIEW",
   },
   {
-    to: "/",
-    icon: "material-symbols:home",
-    text: "Home",
+    to: "/portfolio",
+    icon: "f7:chart-pie",
+    text: "PORTFOLIO",
   },
   {
-    to: "/",
-    icon: "material-symbols:home",
-    text: "Home",
+    to: "/trading",
+    icon: "streamline-freehand:trading-graph",
+    text: "TRADING",
+  },
+  {
+    to: "/quests",
+    icon: "tdesign:dart-board",
+    text: "QUESTS",
+  },
+  {
+    to: "/ranking",
+    icon: "solar:ranking-line-duotone",
+    text: "RANKING",
   },
 ]
 </script>
 
 <style lang="scss">
 .sidebar{
-  max-width: max-content;
+  width: 100%;
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -52,7 +63,24 @@ const links = [
     justify-content: center;
 
     &__link{
-      padding: 10px 5px;
+      display: flex;
+      align-items: center;
+      border-radius: 6px;
+      margin-bottom: 5px;
+      padding: 10px;
+
+      &:last-child{
+        margin-bottom: 0;
+      }
+
+      &.active{
+        background-color: rgba(#00dc82, .2);
+      }
+
+      &:hover{
+        color: #000;
+        background-color: #00dc82;
+      }
     }
   }
 }
