66bf27c5be933d0e0a6f11366c8d806644c0a5b4
GobsRuiz <gbibisruiz@hotmail.com>
2025-11-08 22:39:06 -0300

    feat: add section title component

---
+++ frontend/app/components/UI/SectionTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="UISectionTitle">
+    <slot />
+  </h1>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.UISectionTitle{
+  font-family: 'Britanica Expanded';
+    color: #bdbdbd;
+    font-size: 12px;
+    margin-bottom: 18px;
+}
+</style>
