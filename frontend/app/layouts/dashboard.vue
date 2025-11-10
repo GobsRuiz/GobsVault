e40c05048e64794566085144e1db3b72e04c0842
GobsRuiz <gbibisruiz@hotmail.com>
2025-11-10 15:43:05 -0300

    style: adjust layout

--- frontend/app/layouts/dashboard.vue
+++ frontend/app/layouts/dashboard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="layoutDashboard">
+  <div class="layoutDashboard h-screen box-border py-2">
     <LayoutsSidebar class="col-start-1 row-span-2" />
 
     <LayoutsToolbar class="col-start-2 row-start-1" />
 
-    <main class="col-start-2 row-start-2">
+    <main class="col-start-2 row-start-2 rounded-4xl">
       <slot />
     </main>
   </div>
@@ -16,18 +16,14 @@
 
 <style lang="scss" scoped>
 .layoutDashboard{
-  height: 100vh;
   display: grid;
   grid-template-columns: 70px 1fr;
   grid-template-rows: 70px auto;
-  box-sizing: border-box;
-  padding: 0 10px 10px 0;
 
   main{
     width: 100%;
     height: 100%;
     background-color: var(--ui-bg-accented);
-    border-radius: 30px;
     overflow-y: auto;
     box-sizing: border-box;
     padding: 30px;
